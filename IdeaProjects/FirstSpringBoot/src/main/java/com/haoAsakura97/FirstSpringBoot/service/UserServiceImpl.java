121da758e2e4833ef6546ce5a0825bfcd4de71c5
Auezov Kuanysh <auezov.k@mail.ru>
2022-06-14 22:17:06 +0700

    add @Transactional

--- IdeaProjects/FirstSpringBoot/src/main/java/com/haoAsakura97/FirstSpringBoot/service/UserServiceImpl.java
+++ IdeaProjects/FirstSpringBoot/src/main/java/com/haoAsakura97/FirstSpringBoot/service/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.haoAsakura97.FirstSpringBoot.service;
 import com.haoAsakura97.FirstSpringBoot.model.User;
 import com.haoAsakura97.FirstSpringBoot.repository.UserRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -16,6 +17,7 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional
     public User findById(Long id) {
         User user = userRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
@@ -23,16 +25,19 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional
     public List<User> findAll() {
         return userRepository.findAll();
     }
 
     @Override
+    @Transactional
     public User saveUser(User user) {
         return userRepository.save(user);
     }
 
     @Override
+    @Transactional
     public void deleteById(Long id) {
         userRepository.deleteById(id);
     }
